da7d9b1f616228d1ad5ec134d431d431f46042b7
Alex-home-ubuntu <Gridbugkiller@gmail.com>
2020-09-04 21:29:36 +0700

    Code cleanup

--- car_diagnosis_app/lib/app.dart
+++ car_diagnosis_app/lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:car_diagnosis_app/cars_list/cars_list.dart';
 import 'package:car_diagnosis_app/data_model/entities/car.dart';
 import 'package:car_diagnosis_app/repositories/entities/car.dart';
 import 'package:flutter/cupertino.dart';
@@ -14,92 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: CarListScreen(),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Cars list")),
-      body: Center(
-        child: StreamBuilder<List<Car>>(
-          stream: Provider.of<CarRepository>(context).all,
-          builder: (context, carsSnapshot) {
-            return ListView.separated(
-              itemCount: carsSnapshot.data.length,
-              itemBuilder: (context, itemId) {
-                if (itemId < 0 || itemId > carsSnapshot.data.length) {
-                  return null;
-                }
-
-                return Padding(
-                  padding: const EdgeInsets.all(0.0),
-                  child: _buildCarTile(carsSnapshot.data[itemId]),
-                );
-              },
-              separatorBuilder: (context, index) => Divider(height: 1),
-            );
-          },
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Provider.of<CarRepository>(context, listen: false).create(
-            Car((b) => b..name = "New car"),
-          );
-        },
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-Widget _buildCarTile(Car car) {
-  return ListTile(
-    leading: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(Icons.airport_shuttle),
-      ],
-    ),
-    title: Text(car.name),
-    subtitle: Text(
-        "<car model name, year, VIN code>\n<some details about car defects>"),
-    trailing: Container(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: Text(
-              "42",
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-        ],
-        mainAxisAlignment: MainAxisAlignment.center,
-      ),
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: Colors.blue,
-      ),
-    ),
-    isThreeLine: true,
-    onTap: () {
-      print('>>> ${car.name} was tapped!!!');
-    },
-    onLongPress: () {
-      print('>>> ${car.name} was long pressed!!!');
-    },
-  );
 }
---
+++ car_diagnosis_app/lib/cars_list/cars_list.dart
@@ -0,0 +1,89 @@
+import 'package:car_diagnosis_app/data_model/entities/car.dart';
+import 'package:car_diagnosis_app/repositories/entities/car.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CarListScreen extends StatefulWidget {
+  CarListScreen({Key key}) : super(key: key);
+
+  @override
+  _CarListScreenState createState() => _CarListScreenState();
+}
+
+class _CarListScreenState extends State<CarListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Cars")),
+      body: Center(
+        child: StreamBuilder<List<Car>>(
+          stream: Provider.of<CarRepository>(context).all,
+          builder: (context, carsSnapshot) {
+            return ListView.separated(
+              itemCount: carsSnapshot.data.length,
+              itemBuilder: (context, itemId) {
+                if (itemId < 0 || itemId > carsSnapshot.data.length) {
+                  return null;
+                }
+
+                return Padding(
+                  padding: const EdgeInsets.all(0.0),
+                  child: _buildCarTile(carsSnapshot.data[itemId]),
+                );
+              },
+              separatorBuilder: (context, index) => Divider(height: 1),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Provider.of<CarRepository>(context, listen: false).create(
+            Car((b) => b..name = "New car"),
+          );
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+Widget _buildCarTile(Car car) {
+  return ListTile(
+    leading: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(Icons.airport_shuttle),
+      ],
+    ),
+    title: Text(car.name),
+    subtitle: Text(
+        "<car model name, year, VIN code>\n<some details about car defects>"),
+    trailing: Container(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Text(
+              "42",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+        mainAxisAlignment: MainAxisAlignment.center,
+      ),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.blue,
+      ),
+    ),
+    isThreeLine: true,
+    onTap: () {
+      print('>>> ${car.name} was tapped!!!');
+    },
+    onLongPress: () {
+      print('>>> ${car.name} was long pressed!!!');
+    },
+  );
+}
